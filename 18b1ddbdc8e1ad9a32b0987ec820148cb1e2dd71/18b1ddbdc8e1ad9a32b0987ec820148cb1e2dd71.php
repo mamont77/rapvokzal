86d8261671ae63667f562856291b5808b49345b1
Ruslan Piskarev <ruslan.piskarev@gmail.com>
2015-06-27 14:10:39 +0300

    apply pantheon

--- 18b1ddbdc8e1ad9a32b0987ec820148cb1e2dd71/18b1ddbdc8e1ad9a32b0987ec820148cb1e2dd71.php
+++
@@ -1,6 +0,0 @@
-<?php
-  define('LINKFEED_USER', '18b1ddbdc8e1ad9a32b0987ec820148cb1e2dd71');
-  require_once($_SERVER['DOCUMENT_ROOT'].'/'.LINKFEED_USER.'/linkfeed_articles.php');      
-  $linkfeed = new LinkfeedArticlesClient();
-  echo $linkfeed->return_article();
-?>
